e2ae662f293c08ab2374b744ff3573a3dc8ab958
McNull <noreplies@hotmail.com>
2016-06-23 20:19:04 +0200

    before name convention refactor

--- src/angular-img-cropper.js
+++ src/angular-img-cropper.js
@@ -2,9 +2,11 @@
   'use strict';
 
   var mod = angular.module('angular-img-cropper', []);
-  
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+
   mod.factory('__extends', function () {
-    
+
     var __extends = __extends || function (d, b) {
       for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
       function __() {
@@ -15,8 +17,88 @@
       d.prototype = new __();
     };
 
-    return __extends;    
+    return __extends;
   });
-  
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+
+  mod.directive("imageCropper", function ($document, $window, ImageCropper) {
+    return {
+      scope: {
+        image: "=",
+        croppedImage: "=",
+        cropWidth: "=",
+        cropHeight: "=",
+        keepAspect: "=",
+        touchRadius: "=",
+        cropAreaBounds: "=",
+        minWidth: "=",
+        minHeight: "=",
+        enforceCropAspect: "=",
+        enforceFileType: "@",
+        color: "@"
+      },
+      restrict: "A",
+      link: function (scope, element, attrs) {
+        var crop, destroyed = false;
+        scope.color = scope.color || 'rgba(90,90,90,0.75)';
+
+        scope.$on('$destroy', function () {
+          destroyed = true;
+        });
+
+        function setup(newValue, oldValue) {
+          if (crop && newValue === oldValue) {
+            return;
+          }
+
+          var canvas = element[0];
+          var width = scope.cropWidth;
+          var height = scope.cropHeight;
+          var keepAspect = scope.keepAspect;
+          var touchRadius = scope.touchRadius;
+          var oldImage = crop && crop.srcImage;
+
+          crop = new ImageCropper(canvas, canvas.width / 2 - width / 2, canvas.height / 2 - height / 2, width, height, keepAspect, touchRadius, scope, attrs);
+
+          $(canvas).data('crop.angular-img-cropper', crop);
+
+          if (oldImage) {
+            crop.setImage(oldImage, scope.image.fileType);
+          } else {
+            load(scope.image);
+          }
+        }
+
+        function load(newValue) {
+          if (!newValue) {
+            return;
+          }
+
+          var imageObj = new Image();
+
+          if (attrs.cors !== undefined && attrs.cors !== "no") {
+            imageObj.crossOrigin = "Anonymous";
+          }
+
+          imageObj.addEventListener("load", function () {
+            crop.setImage(imageObj, newValue.fileType);
+            scope.$apply();
+          }, false);
+          imageObj.src = newValue.imageData;
+        }
+
+        scope.$watch('cropWidth', setup);
+        scope.$watch('cropHeight', setup);
+        scope.$watch('keepAspect', setup);
+        scope.$watch('touchRadius', setup);
+
+        scope.$watch('image', load);
+      }
+    };
+  });
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+
 })(angular, angular.element);
 
